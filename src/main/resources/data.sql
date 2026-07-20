a4c4c56577b81c77e787da073aefa6f877673e74
Юрий Дубровских <u.dubrovskikh@planfact.io>
2026-07-20 16:40:00 +0500

    Изменение настроек для подключения web flux

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,38 +1,9 @@
-
--- Вставка тестовых товаров
-INSERT INTO item (title, description, img_path, price) VALUES
-                                                           ('Смартфон Xiaomi Redmi Note 11',
-                                                            'Смартфон с 6.43-дюймовым AMOLED-дисплеем, 4 ГБ ОЗУ и 128 ГБ встроенной памяти, процессор Snapdragon 680',
-                                                            '/images/xiaomi_redmi_note_11.jpg',
-                                                            15999),
-
-                                                           ('Ноутбук Lenovo IdeaPad 3',
-                                                            '15.6-дюймовый ноутбук с процессором Intel Core i5, 8 ГБ ОЗУ, 256 ГБ SSD, Windows 11',
-                                                            '/images/lenovo_ideapad_3.jpg',
-                                                            34999),
-
-                                                           ('Наушники Sony WH-1000XM5',
-                                                            'Беспроводные наушники с активным шумоподавлением, до 30 часов работы, поддерживают LDAC',
-                                                            '/images/sony_wh_1000xm5.jpg',
-                                                            29999),
-
-                                                           ('Клавиатура Logitech MX Keys',
-                                                            'Беспроводная клавиатура с подсветкой, эргономичный дизайн, подключение до 3 устройств',
-                                                            '/images/logitech_mx_keys.jpg',
-                                                            8999),
-
-                                                           ('Монитор Samsung Odyssey G5',
-                                                            '27-дюймовый монитор с разрешением QHD (2560x1440), частотой 144 Гц, временем отклика 1 мс',
-                                                            '/images/samsung_odyssey_g5.jpg',
-                                                            25999);
-
--- Вставка данных в корзину (связь с первым товаром - Xiaomi Redmi Note 11)
-INSERT INTO cart (item_id, count) VALUES
-    (1, 2);  -- 2 штуки Xiaomi Redmi Note 11 в корзине
-
--- Вставка заказа
+INSERT INTO item (title, description, img_path, price) VALUES ('Смартфон Xiaomi Redmi Note 11', 'Смартфон с 6.43-дюймовым AMOLED-дисплеем, 4 ГБ ОЗУ и 128 ГБ встроенной памяти, процессор Snapdragon 680', '/images/xiaomi_redmi_note_11.jpg', 15999);
+INSERT INTO item (title, description, img_path, price) VALUES ('Ноутбук Lenovo IdeaPad 3', '15.6-дюймовый ноутбук с процессором Intel Core i5, 8 ГБ ОЗУ, 256 ГБ SSD, Windows 11', '/images/lenovo_ideapad_3.jpg', 34999);
+INSERT INTO item (title, description, img_path, price) VALUES ('Наушники Sony WH-1000XM5', 'Беспроводные наушники с активным шумоподавлением, до 30 часов работы, поддерживают LDAC', '/images/sony_wh_1000xm5.jpg', 29999);
+INSERT INTO item (title, description, img_path, price) VALUES ('Клавиатура Logitech MX Keys', 'Беспроводная клавиатура с подсветкой, эргономичный дизайн, подключение до 3 устройств', '/images/logitech_mx_keys.jpg', 8999);
+INSERT INTO item (title, description, img_path, price) VALUES ('Монитор Samsung Odyssey G5', '27-дюймовый монитор с разрешением QHD (2560x1440), частотой 144 Гц, временем отклика 1 мс', '/images/samsung_odyssey_g5.jpg', 25999);
+INSERT INTO cart (item_id, count) VALUES (1, 2);
 INSERT INTO orders (total_sum) VALUES (41998);
-
-INSERT INTO order_items (order_id, item_id) VALUES
-                                                (1, 1),  -- Смартфон Xiaomi Redmi Note 11
-                                                (1, 5);  -- Монитор Samsung Odyssey G5
+INSERT INTO order_items (order_id, item_id, count) VALUES (1, 1, 1);
+INSERT INTO order_items (order_id, item_id, count) VALUES (1, 5, 1);
