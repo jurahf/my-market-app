eb4cc7fae0560cc25c49bdf1c51e305669c362c0
Юрий Дубровских <u.dubrovskikh@planfact.io>
2026-08-06 13:21:45 +0500

    PaymentService на AtomicLong

--- paymentservice/src/main/java/org/yap/paymentservice/services/PaymentService.java
+++ paymentservice/src/main/java/org/yap/paymentservice/services/PaymentService.java
@@ -4,16 +4,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @Service
 public class PaymentService {
 
-    private long startSumStub = 10_000L;
+    private AtomicLong startSumStub = new AtomicLong(10_000L);
 
     public Mono<Long> getBalance() {
-        return Mono.just(startSumStub);
+        return Mono.just(startSumStub.get());
     }
 
     public Mono<Long> decBalance(Long decValue) {
-        return Mono.just(startSumStub - decValue);
+        return Mono.just(startSumStub.addAndGet(-decValue));
     }
 }
